1c450b47a77465cb2c271cb30fe5af04d6dadc1c
Vinicius Motta <vinicius.costa@trescon.com.br>
2024-06-04 12:08:59 -0300

    adding new path and image to test

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <div>Contact</div>
+      <Image src="http://github.com/v-motta.png" alt="" />
+    </div>
+  )
+}
