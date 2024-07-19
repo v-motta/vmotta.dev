c2116eb14c50bbca21679d09e31d1508e955c473
v-motta <vinicius.costa@trescon.com.br>
2024-07-19 03:29:27 -0300

    feat: projects grid

---
+++ src/app/(portfolio)/projects/card-project.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import { Button } from '../../components/button'
+import { GithubIcon } from '../../components/icons/github-icon'
+import Image from 'next/image'
+import { Globe } from 'lucide-react'
+
+export interface CardProjectProps {
+  image: string
+  title: string
+  subtitle: string
+  slug: string
+  github?: string
+  deployUrl?: string
+}
+
+export function CardProject(props: CardProjectProps) {
+  return (
+    <div className="flex flex-col gap-5 rounded-2xl border border-zinc-200 p-5">
+      <Image
+        src={props.image}
+        alt=""
+        width={500}
+        height={500}
+        quality={100}
+        className="aspect-video h-32 rounded-lg object-cover object-center"
+      />
+      <div className="flex flex-1 flex-col gap-3 text-center lg:text-start">
+        <h1 className="font-mono text-xl font-bold">{props.title}</h1>
+        <p className="line-clamp-2 text-zinc-300">{props.subtitle}</p>
+      </div>
+      <div className="flex gap-5">
+        <Button variant="outline" className="flex-1" asChild>
+          <Link href={`projects/${props.slug}`}>Show more</Link>
+        </Button>
+        {props.github && (
+          <Button variant="outline" className="px-2" asChild>
+            <Link href={props.github} target="_blank" rel="noopener noreferrer">
+              <GithubIcon width={22} height={22} className="fill-zinc-50" />
+            </Link>
+          </Button>
+        )}
+        {props.deployUrl && (
+          <Button variant="outline" className="px-2" asChild>
+            <Link
+              href={props.deployUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Globe width={22} height={22} className="text-zinc-50" />
+            </Link>
+          </Button>
+        )}
+      </div>
+    </div>
+  )
+}
--- src/app/(portfolio)/projects/page.tsx
+++ src/app/(portfolio)/projects/page.tsx
@@ -1,7 +1,20 @@
+import data from '@/app/data.json'
+import { CardProject } from './card-project'
+
 export default function ProjectsPage() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="grid gap-5 py-8 md:grid-cols-2 lg:grid-cols-3 lg:py-0 xl:grid-cols-4 2xl:grid-cols-5">
+      {data.projects.map((project) => (
+        <CardProject
+          key={project.id}
+          image="https://github.com/v-motta.png"
+          title={project.title}
+          subtitle={project.subtitle}
+          slug={project.slug}
+          github={project.github}
+          deployUrl={project.deplyUrl}
+        />
+      ))}
     </div>
   )
 }
