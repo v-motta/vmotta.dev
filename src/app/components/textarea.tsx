ab57e0faa4c986b2cb72cd505ef6fd7c9e8d32ca
v-motta <vinicius.costa@trescon.com.br>
2024-07-25 01:53:51 -0300

    feat: new textarea component

---
+++ src/app/components/textarea.tsx
@@ -0,0 +1,26 @@
+import { forwardRef, type ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export interface TextareaProps extends ComponentProps<'textarea'> {
+  withError?: boolean
+}
+
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, withError, ...rest }, ref) => {
+    return (
+      <textarea
+        ref={ref}
+        className={twMerge(
+          'flex min-h-[120px] w-full resize-y items-center gap-2 rounded-lg border border-zinc-400 bg-transparent px-3 py-1 outline-none 2xl:py-2',
+          'focus-within:border-zinc-100 focus-within:ring-4 focus-within:ring-zinc-500/50',
+          withError &&
+            'border-red-500 focus-within:border-red-500 focus-within:ring-4 focus-within:ring-red-100',
+          className,
+        )}
+        {...rest}
+      />
+    )
+  },
+)
+
+Textarea.displayName = 'Textarea'
