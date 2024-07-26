b98efd381cba33b23a7d0b5f7a57789d61aee081
v-motta <vinicius.costa@trescon.com.br>
2024-07-26 16:31:31 -0300

    fix: focus when error field

--- src/app/components/textarea.tsx
+++ src/app/components/textarea.tsx
@@ -14,7 +14,7 @@ export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
           'flex min-h-[120px] w-full resize-y items-center gap-2 rounded-lg border border-zinc-400 bg-transparent px-3 py-1 outline-none 2xl:py-2',
           'focus-within:border-zinc-100 focus-within:ring-4 focus-within:ring-zinc-500/50',
           withError &&
-            'border-red-500 focus-within:border-red-500 focus-within:ring-4 focus-within:ring-red-100',
+            'border-red-500 focus-within:border-red-500 focus-within:ring-4 focus-within:ring-red-500/40',
           className,
         )}
         {...rest}
