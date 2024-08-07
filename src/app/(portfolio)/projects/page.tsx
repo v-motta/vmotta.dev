92a21a475df64aea0c23d7c19856d88178332cd2
v-motta <vinicius.costa@trescon.com.br>
2024-08-06 22:53:20 -0300

    fix: spacing y

--- src/app/(portfolio)/projects/page.tsx
+++ src/app/(portfolio)/projects/page.tsx
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
   return (
     <div className="xl:min-h-[calc(100vh-6rem-6rem)]">
       <Title title="Projects" className="mt-8 lg:mt-0" />
-      <div className="grid gap-5 py-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:py-0 2xl:grid-cols-5">
+      <div className="grid gap-5 py-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {data.projects.map((project) => (
           <CardProject
             key={project.id}
