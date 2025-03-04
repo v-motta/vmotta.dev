655f0c50283ff69a7852c7d3eed5921e8253183c
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-03-04 01:52:08 +0100

    feat: all projects and certificates page

---
+++ src/app/certificates/page.tsx
@@ -0,0 +1,46 @@
+import { getCleanText } from '@/lib/clean-text'
+import { prisma } from '@/lib/prisma-client'
+import { CardCertificate } from '../(home)/(sections)/(latest-certificates)/card-certificate'
+
+export default async function Certificates() {
+  const allCertificates = await prisma.certificate.findMany({
+    select: {
+      id: true,
+      title: true,
+      slug: true,
+      imageUrl: true,
+      issueDate: true,
+      technologies: true,
+      hourDuration: true,
+      company: true,
+    },
+    orderBy: { issueDate: 'desc' },
+  })
+
+  const allCertificatesSimplified = allCertificates.map((certificate) => ({
+    ...certificate,
+    technologies: certificate.technologies.map((tech) =>
+      getCleanText(tech.name)
+    ),
+  }))
+
+  return (
+    <div className="flex flex-col gap-10 bg-topography px-5 py-10 md:px-20 lg:px-5 2xl:px-36 2xl:py-20">
+      <div className="flex flex-col items-center justify-between gap-y-4 sm:flex-row">
+        <h1 className="font-extrabold text-2xl">All my certificates</h1>
+
+        <p className="text-zinc-400">
+          Showing{' '}
+          <span className="font-extrabold">{allCertificates.length}</span>{' '}
+          certificates
+        </p>
+      </div>
+
+      <div className="grid gap-x-6 gap-y-5 sm:grid-cols-2 lg:grid-cols-3">
+        {allCertificatesSimplified.map((certificate) => (
+          <CardCertificate key={certificate.id} certificate={certificate} />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,36 @@
-export default function Projects() {
-  return <div>Projects</div>
+import { prisma } from '@/lib/prisma-client'
+import { CardProject } from '../(home)/(sections)/(top-projects)/card-project'
+
+export default async function Projects() {
+  const allProjects = await prisma.project.findMany({
+    select: {
+      id: true,
+      title: true,
+      subtitle: true,
+      slug: true,
+      github: true,
+      imagesUrl: true,
+      createdAt: true,
+    },
+    orderBy: [{ topProject: 'desc' }, { createdAt: 'desc' }],
+  })
+
+  return (
+    <div className="flex flex-col gap-10 bg-topography px-5 py-10 md:px-20 lg:px-5 2xl:px-36 2xl:py-20">
+      <div className="flex flex-col items-center justify-between gap-y-4 sm:flex-row">
+        <h1 className="font-extrabold text-2xl">All my projects</h1>
+
+        <p className="text-zinc-400">
+          Showing <span className="font-extrabold">{allProjects.length}</span>{' '}
+          projects
+        </p>
+      </div>
+
+      <div className="grid gap-x-6 gap-y-5 sm:grid-cols-2 lg:grid-cols-3">
+        {allProjects.map((project) => (
+          <CardProject key={project.id} project={project} />
+        ))}
+      </div>
+    </div>
+  )
 }
