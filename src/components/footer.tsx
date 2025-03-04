43912fb216d8912d1bfd067996bb4893cd1c3eae
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-03-04 01:51:47 +0100

    feat: footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex h-16 items-center justify-center border-t border-t-zinc-800 bg-background/95 shadow-md xl:px-36">
+      <p className="text-sm text-zinc-400">
+        Made with ❤️ by <a href="https://vmotta.dev">vmotta</a>
+      </p>
+    </footer>
+  )
+}
