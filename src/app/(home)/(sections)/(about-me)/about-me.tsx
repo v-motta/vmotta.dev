4f33578197d57385f1ee39bac012bce3d251bd41
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-03-05 02:43:23 +0100

    feat: add placeholder text info

--- src/app/(home)/(sections)/(about-me)/about-me.tsx
+++ src/app/(home)/(sections)/(about-me)/about-me.tsx
@@ -2,12 +2,37 @@ export function AboutMe() {
   return (
     <section
       id="about_me"
-      className="bg-topography px-5 py-20 md:px-20 lg:px-5 2xl:min-h-[calc(100vh-4rem)] 2xl:px-36"
+      className="flex flex-col bg-topography px-5 py-20 md:px-20 lg:px-5 2xl:min-h-[calc(100vh-4rem)] 2xl:px-36"
     >
-      <div className="flex h-full flex-col gap-y-8 rounded-2xl border border-primary bg-background/75 p-6">
+      <div className="relative flex flex-1 flex-col gap-y-8 rounded-2xl border border-primary bg-background/75 p-6">
         <h1 className="text-center font-extrabold text-xl md:text-start md:text-3xl">
           About me
         </h1>
+
+        <p className="text-muted-foreground">
+          Illo dignissimos error adipisci quod voluptas dolor est quisquam non,
+          ad deleniti doloribus. Obcaecati, explicabo velit! Id eius qui
+          voluptatem totam! Animi magni tempore omnis voluptatibus cumque, autem
+          quis minus facere ab, sit officia necessitatibus, rerum at. Molestias
+          mollitia, assumenda nobis consequuntur ipsam adipisci explicabo omnis
+          quisquam dolores nostrum ullam impedit! Reprehenderit, repellendus.
+          Accusantium nostrum dicta, ab vel adipisci commodi? Corrupti quis iste
+          accusamus sit consequatur quod, architecto vero saepe reprehenderit in
+          porro quam provident soluta illo laboriosam sunt, obcaecati
+          dignissimos? Voluptas obcaecati architecto mollitia quasi adipisci?
+          Expedita ex aperiam facere, unde repellat perferendis hic atque illum
+          maxime magnam omnis molestias, magni laudantium, animi eius ipsam
+          possimus qui. Ratione, velit expedita. Pariatur tempora nulla expedita
+          beatae, qui labore similique quo vero laboriosam porro aspernatur ex
+          quis cumque saepe? Nihil dolorum magni, odio quidem animi iusto,
+          nesciunt ex, sequi ipsa cupiditate error. Enim vitae tenetur saepe
+          voluptatum quas porro, laborum placeat dignissimos cumque quae.
+        </p>
+
+        <p className="-translate-x-1/2 absolute top-1/2 left-1/2 text-nowrap bg-background/80 p-4 text-3xl text-muted-foreground uppercase lg:text-5xl">
+          Placeholder text
+        </p>
+
         {/* <p className="text-balance text-2xl text-zinc-400">
           I am a passionate software engineer with a strong foundation in
           computer science and a diverse skill set in full-stack development. My
@@ -30,12 +55,6 @@ export function AboutMe() {
           innovative solutions that make a positive impact on users and
           businesses alike.
         </p> */}
-
-        <div className="flex h-full items-center justify-center">
-          <p className="text-lg text-muted-foreground md:text-2xl">
-            Coming soon
-          </p>
-        </div>
       </div>
     </section>
   )
