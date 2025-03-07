99514fd153c27cb87ba68ab2b48bf7ca4c273b84
Vinicius Motta <viniciusmotta0806@gmail.com>
2025-03-07 18:29:28 +0100

    fix: add animate-heartbeat style to tailwind

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,12 @@
+import { Heart } from 'lucide-react'
+
 export function Footer() {
   return (
     <footer className="flex h-16 items-center justify-center border-t border-t-border bg-background/95 shadow-md xl:px-36">
-      <p className="text-muted-foreground text-sm">
-        Made with ❤️ by <a href="https://vmotta.dev">vmotta</a>
+      <p className="flex flex-nowrap items-end gap-2 text-muted-foreground text-sm">
+        Made with{' '}
+        <Heart className="size-5 animate-heartbeat fill-red-500 text-red-500" />{' '}
+        by <a href="https://vmotta.dev">vmotta</a>
       </p>
     </footer>
   )
